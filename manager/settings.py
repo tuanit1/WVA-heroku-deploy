9ce7221e6c46c1cf8dc0722b9d7958c150748cbb
Do Thanh Tuan <81170150+tuanit1@users.noreply.github.com>
2022-06-05 00:25:29 +0700

    commit

--- manager/settings.py
+++ manager/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 MEDIA_URL = '/styles/images/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'home/media/')
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
